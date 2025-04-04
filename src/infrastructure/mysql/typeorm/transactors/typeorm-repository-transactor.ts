f3a23ea4dfe8983a64db9e0cd1d458f044ff7f76
minericefield <minericefield@gmail.com>
2025-04-05 00:08:33 +0900

    Add: transactor

--- src/infrastructure/mysql/typeorm/transactors/typeorm-repository-transactor.ts
+++ src/infrastructure/mysql/typeorm/transactors/typeorm-repository-transactor.ts
@@ -2,13 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 
 import { RepositoryTransactor } from '../../../../application/shared/repository-transactor.';
+import { AlsThreadContext } from '../../../node-async-hooks/als-thread-context';
+
+export const ENTITY_MANAGER_THREAD_CONTEXT_KEY = 'ENTITY_MANAGER';
 
 @Injectable()
 export class TypeormRepositoryTransactor implements RepositoryTransactor {
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly alsThreadContext: AlsThreadContext,
+  ) {}
 
-  async handle<T>(manipulation: () => Promise<T>): Promise<T> {
-    // Simple transactions seem to work even without entity manager.
-    return this.dataSource.transaction(manipulation);
+  async handle<T>(
+    manipulation: () => Promise<T>,
+  ): ReturnType<typeof manipulation> {
+    return this.dataSource.transaction((entityManager) => {
+      this.alsThreadContext.set(
+        ENTITY_MANAGER_THREAD_CONTEXT_KEY,
+        entityManager,
+      );
+
+      return manipulation();
+    });
   }
 }
